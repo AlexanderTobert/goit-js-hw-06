91f80144169bde4eafbbb2b4ee133aaac59e33a3
AlexanderTobert <tobert.sasha@gmail.com>
2022-08-31 16:19:01 +0300

    update task 5 & task 6

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,8 @@ const inputRef = document.querySelector('#name-input');
 const spanRef = document.querySelector('#name-output');
 
 inputRef.addEventListener('input', () => {
-    if (inputRef.value == '') {
-    spanRef.textContent = "Anonymous";
-} else {
-    spanRef.textContent = inputRef.value;
-}
+    if (inputRef.value === '') {
+        spanRef.textContent = "Anonymous";
+        return;
+} spanRef.textContent = inputRef.value;
 })
--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,10 @@
 const inputRef = document.querySelector('#validation-input');
 
 inputRef.addEventListener('blur', () => {
-    const minLength = inputRef.dataset.length;
+    const minLength = parseInt(inputRef.dataset.length);
     // console.log(minLength);
     // console.log(inputRef.value.length);
+
     if (inputRef.value.length >= minLength) {
         inputRef.classList.remove("invalid");
         inputRef.classList.add("valid");

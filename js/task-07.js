c1e90a4406f17f951e51fd45122c70c2eaa11e1e
AlexanderTobert <tobert.sasha@gmail.com>
2022-09-01 13:35:37 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,9 +3,19 @@ const spanRef = document.querySelector('#text');
 
 // console.dir(spanRef);
 
-spanRef.style.fontSize = inputRef.value + "px";
+// spanRef.style.fontSize = inputRef.value + "px";
 
-inputRef.addEventListener('input', () => {
-    // console.log(inputRef.value);
+// inputRef.addEventListener('input', () => {
+//     // console.log(inputRef.value);
+//     spanRef.style.fontSize = inputRef.value + "px";
+// })
+
+
+const ref = function () {
     spanRef.style.fontSize = inputRef.value + "px";
+}
+ref();
+
+inputRef.addEventListener('input', () => { 
+    ref();
 })

5631e4c571434408fb542b4c34d455a2160fe6cc
AlexanderTobert <tobert.sasha@gmail.com>
2022-09-01 12:43:56 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -5,21 +5,28 @@ console.log('Number of categories:', ulRefs.length);
 // const liRefs = document.querySelector('.item ul li');
 // console.log(ulRefs);
 
-for (let elem of ulRefs) {
-    // console.log(elem);
-    // const h2Refs = elem.querySelector('.item > h2');
-    // const liRefs = elem.querySelectorAll('.item > ul > li');
-
-    const h2Refs_El = elem.firstElementChild;
-    const liRefs_El = elem.lastElementChild;
-    // console.log(h2Refs);
-    // h2Refs.textContent = "12345678"
-    // console.log('Category:', h2Refs.textContent);
-    // console.log('Elements:', liRefs.length);
-
+ulRefs.forEach(element => {
+    const h2Refs_El = element.firstElementChild;
+    const liRefs_El = element.lastElementChild;
     console.log('Category:', h2Refs_El.textContent);
-    // console.log('Elements:', liRefs_El.childElementCount);
     console.log('Elements:', liRefs_El.children.length);
-  }
+})
+
+// for (let elem of ulRefs) {
+//     // console.log(elem);
+//     // const h2Refs = elem.querySelector('.item > h2');
+//     // const liRefs = elem.querySelectorAll('.item > ul > li');
+
+//     const h2Refs_El = elem.firstElementChild;
+//     const liRefs_El = elem.lastElementChild;
+//     // console.log(h2Refs);
+//     // h2Refs.textContent = "12345678"
+//     // console.log('Category:', h2Refs.textContent);
+//     // console.log('Elements:', liRefs.length);
+
+//     console.log('Category:', h2Refs_El.textContent);
+//     // console.log('Elements:', liRefs_El.childElementCount);
+//     console.log('Elements:', liRefs_El.children.length);
+//   }
 
 // console.log(liRefs);

8b95a529b05c9a266632c76165a7fbb7e97e469c
AlexanderTobert <tobert.sasha@gmail.com>
2022-09-01 13:16:45 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,23 @@
 const inputRef = document.querySelector('#name-input');
 const spanRef = document.querySelector('#name-output');
 
+
+// 1 метод
+
+// inputRef.addEventListener('input', () => {
+//     if (inputRef.value === '') {
+//         spanRef.textContent = "Anonymous";
+//         return;
+// } spanRef.textContent = inputRef.value;
+// })
+
+//2 метод
+// inputRef.addEventListener('input', () => {
+//     inputRef.value === '' ? spanRef.textContent = "Anonymous" : spanRef.textContent = inputRef.value;
+// })
+
+//3 метод
 inputRef.addEventListener('input', () => {
-    if (inputRef.value === '') {
-        spanRef.textContent = "Anonymous";
-        return;
-} spanRef.textContent = inputRef.value;
+    console.log(inputRef.value)
+    spanRef.textContent = inputRef.value || "Anonymous";
 })
